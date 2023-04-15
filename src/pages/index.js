4a6d26b44b8e014d5893104e660378a2afdf1f6a
luis.poveda93 <luis.poveda9321@gmail.com>
2023-04-15 23:15:43 +0200

    first viewer test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,67 @@
-import React from 'react';
-import { Document, Page } from 'react-pdf';
+import React, { useState } from 'react';
+import { Document, Page, Outline } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 
 
 export default function Test() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+    setPageNumber(1);
+  }
+
+  function changePage(offset) {
+    setPageNumber(prevPageNumber => prevPageNumber + offset);
+  }
+
+  function previousPage() {
+    changePage(-1);
+  }
+
+  function nextPage() {
+    changePage(1);
+  }
+
+  function onItemClick({ pageNumber: itemPageNumber }) {
+    setPageNumber(itemPageNumber);
+  }
+
   return (
-    <Document file={'sample.pdf'}
-      options={{
-        cMapUrl: 'cmaps/',
-        cMapPacked: true,
-        standardFontDataUrl: 'standard_fonts/',
-      }}
-    >
-      <Page pageNumber={1} />
-    </Document>
+    <>
+      <Document 
+        file={'sample.pdf'}
+        onLoadSuccess={onDocumentLoadSuccess}
+        options={{
+          cMapUrl: 'cmaps/',
+          cMapPacked: true,
+          standardFontDataUrl: 'standard_fonts/',
+        }}
+      >
+        <Outline onItemClick={onItemClick} />
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <div>
+        <p>
+          Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
+        </p>
+        <button
+          type="button"
+          disabled={pageNumber <= 1}
+          onClick={previousPage}
+        >
+          Previous
+        </button>
+        <button
+          type="button"
+          disabled={pageNumber >= numPages}
+          onClick={nextPage}
+        >
+          Next
+        </button>
+      </div>
+    </>
   );
 }
